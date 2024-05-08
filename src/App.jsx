104273fbe3cd62dc5d44c644af491cfb5fa6a377
RohanGorla <rohangorla2010@gmail.com>
2024-05-08 17:22:16 +0530

    Finish hero section version 1

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+import Header from "./hero";
+import Content from "./content";
+import Footer from "./footer";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/hero.jsx
@@ -0,0 +1,37 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div>
+        <nav className="nav">
+          <span>Home</span>
+          <span>Contact</span>
+          <span>Resume</span>
+          <span>Projects</span>
+        </nav>
+      </div>
+      <div className="hero">
+        <div className="title-container">
+          <div className="title">
+            <h1>
+              Hi, I am <span style={{ color: "red" }}>Rohan</span>
+            </h1>
+            <h3>
+              I'm a <span style={{ color: "red" }}>Frontend</span> Web Developer
+            </h3>
+            <p>
+              With a passion to build websites that are interactive & user
+              friendly and always striving to learn more and get better to stay
+              udated with the current technologies.
+            </p>
+          </div>
+        </div>
+        <div className="image">
+          <div className="image-background"></div>
+          <img src="../src/assets/profile_image.jpg"></img>
+        </div>
+      </div>
+    </div>
+  );
+}
