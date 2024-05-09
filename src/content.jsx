c54d1966fa3aaa8f32d25100b64b6605d65b0f02
RohanGorla <rohangorla2010@gmail.com>
2024-05-09 06:07:19 +0530

    update skills section

--- src/content.jsx
+++ src/content.jsx
@@ -48,6 +48,38 @@ export default function Content() {
               <img src="https://ih1.redbubble.net/image.404031065.2191/st,small,507x507-pad,600x600,f8f8f8.u1.jpg"></img>
               <p>Node.js</p>
             </div>
+            <div className="skill">
+              <img src="https://images.credly.com/images/1c2c86e1-16ce-4e4d-a425-d1ac96bb026d/express.png"></img>
+              <p>express.js</p>
+            </div>
+            <div className="skill">
+              <img src="https://static-00.iconduck.com/assets.00/npm-icon-2048x2048-8sw7kisf.png"></img>
+              <p>npm</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="content Database">
+        <div className="labels">
+          <div className="skills-label">
+            <h3>General Skills & Tools</h3>
+            <div className="line"></div>
+          </div>
+        </div>
+        <div className="display">
+          <div className="skills-main">
+            <div className="skill">
+              <img src="https://symbols.getvecta.com/stencil_27/81_sql-database.712f6a994f.jpg"></img>
+              <p>SQL</p>
+            </div>
+            <div className="skill">
+              <img src="https://pbs.twimg.com/profile_images/1255113654049128448/J5Yt92WW_400x400.png"></img>
+              <p>MySQL</p>
+            </div>
+            <div className="skill">
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSEQSWe9Z-7Qlr0Z_LQDj9X4LS5QVEmPbtJdtIHhEDRKw&s"></img>
+              <p>PostgreSQL</p>
+            </div>
           </div>
         </div>
       </div>
@@ -61,28 +93,20 @@ export default function Content() {
         <div className="display">
           <div className="skills-main">
             <div className="skill">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR3bnAudKmxrYl1uhHuNV3VjSp0fYbVKhyQTYgWKFz4LA&s"></img>
-              <p>HTML</p>
+              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEkW5QVyzoVT_A8dA6aG4Y6mf_q64EjDpnpRxm49mvUg&s"></img>
+              <p>Git</p>
             </div>
             <div className="skill">
-              <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT_2McwGBo3NLAbkv7kolqy4OqOXKQsK0untMBWx4i6sg&s"></img>
-              <p>CSS</p>
+              <img src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png"></img>
+              <p>Github</p>
             </div>
             <div className="skill">
-              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/99/Unofficial_JavaScript_logo_2.svg/1024px-Unofficial_JavaScript_logo_2.svg.png"></img>
-              <p>Java Script</p>
+              <img src="https://www.pngitem.com/pimgs/m/290-2909960_responsive-web-design-icon-web-design-hd-png.png"></img>
+              <p>Responsive Design</p>
             </div>
             <div className="skill">
-              <img src="https://cdn.worldvectorlogo.com/logos/react-1.svg"></img>
-              <p>React.js</p>
-            </div>
-            <div className="skill">
-              <img src="https://ih1.redbubble.net/image.404031065.2191/st,small,507x507-pad,600x600,f8f8f8.u1.jpg"></img>
-              <p>Node.js</p>
-            </div>
-            <div className="skill">
-              <img src="https://www.drupal.org/files/project-images/screenshot_361.png"></img>
-              <p>Tailwind Css</p>
+              <img src="https://pbs.twimg.com/profile_images/2199543684/ejs_400x400.png"></img>
+              <p>Embedded JS</p>
             </div>
           </div>
         </div>
