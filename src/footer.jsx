08f398bc83c2c9f00be2350dae3172bbf55763ff
RohanGorla <rohangorla2010@gmail.com>
2024-05-10 00:47:02 +0530

    add footer and update hero section

--- src/footer.jsx
+++ src/footer.jsx
@@ -1,3 +1,36 @@
+import "./footer.css";
+
 export default function Footer() {
-  return <>{/* This is footer section */}</>;
+  return (
+    <div className="Footer-main">
+      <div className="Footer">
+        <div className="contact">
+          <h2>Contact me</h2>
+          <div className="contact-apps">
+            <a href="https://www.linkedin.com/in/r0hang0rla/" target="_blank">
+              <img src="https://images.rawpixel.com/image_png_800/czNmcy1wcml2YXRlL3Jhd3BpeGVsX2ltYWdlcy93ZWJzaXRlX2NvbnRlbnQvbHIvdjk4Mi1kNS0xMF8xLnBuZw.png"></img>
+            </a>
+            <a href="mailto:rohangorla2010@gmail.com">
+              <img src="https://static.vecteezy.com/system/resources/previews/022/484/516/original/google-mail-gmail-icon-logo-symbol-free-png.png"></img>
+            </a>
+            <a
+              href="https://www.naukri.com/mnjuser/profile?id=&altresid"
+              target="_blank"
+            >
+              <img src="https://downloadr2.apkmirror.com/wp-content/uploads/2023/04/36/643050b590a0c.png"></img>
+            </a>
+          </div>
+        </div>
+        <div className="work">
+          <h2>My work</h2>
+          <div className="work-apps">
+            <a href="https://github.com/RohanGorla" target="_blank">
+              <img src="https://cdns.iconmonstr.com/wp-content/releases/preview/2012/240/iconmonstr-github-1.png"></img>
+            </a>
+          </div>
+        </div>
+      </div>
+      <p>created by <span className="name">Rohan Gorla</span></p>
+    </div>
+  );
 }
--- src/hero.jsx
+++ src/hero.jsx
@@ -1,38 +1,49 @@
 import "./header.css";
-import profile_image from './profile_image.jpg';
+import profile_image from "./profile_image.jpg";
 
 export default function Header() {
   return (
-    <div className="header">
+    <>
       <div className="navBar">
         <nav className="nav">
-          <span>Home</span>
-          <span>Contact</span>
-          <span>Resume</span>
-          <span>Projects</span>
+          <a href="https://rohan-official-portfolio.netlify.app/">
+            <span>Home</span>
+          </a>
+          <a href="" target="_blank">
+            <span>Contact</span>
+          </a>
+          <a href="" target="_blank">
+            <span>Projects</span>
+          </a>
+          <a href="" target="_blank">
+            <span>Resume</span>
+          </a>
         </nav>
       </div>
-      <div className="hero">
-        <div className="title-container">
-          <div className="title">
-            <h1>
-              Hi, I am <span style={{ color: "red" }}>Rohan</span>
-            </h1>
-            <h3>
-              I'm a <span style={{ color: "red" }}>Frontend</span> Web Developer
-            </h3>
-            <p>
-              With a passion to build websites that are interactive & user
-              friendly and always striving to learn more and get better to stay
-              udated with the current technologies.
-            </p>
+      <div className="header">
+        <div className="hero">
+          <div className="title-container">
+            <div className="title">
+              <h1>
+                Hi, I am <span style={{ color: "red" }}>Rohan</span>
+              </h1>
+              <h3>
+                I'm a <span style={{ color: "red" }}>Frontend</span> Web
+                Developer
+              </h3>
+              <p>
+                With a passion to build websites that are interactive & user
+                friendly and always striving to learn more and get better to
+                stay udated with the current technologies.
+              </p>
+            </div>
+          </div>
+          <div className="image">
+            <div className="image-background"></div>
+            <img src={profile_image}></img>
           </div>
         </div>
-        <div className="image">
-          <div className="image-background"></div>
-          <img src={profile_image}></img>
-        </div>
       </div>
-    </div>
+    </>
   );
 }
