4fedf7cc94917e2bbddbafbac8d99fe434943832
RohanGorla <rohangorla2010@gmail.com>
2024-06-16 19:08:08 +0530

    add hamburger icon for mobile and remove few css errors

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useRef } from "react";
-import { Link } from "react-router-dom";
+import { useRef, useState } from "react";
+import { Link, useSearchParams } from "react-router-dom";
 import "./App.css";
 import Header from "./components/hero";
 import Content from "./components/content";
@@ -10,31 +10,58 @@ import resume from "./RESUME_2.pdf";
 function App() {
   const contactRef = useRef(null);
   const projectRef = useRef(null);
+  const [showNav, setShowNav] = useState(false);
   return (
     <>
       <div className="navBar">
-        <nav className="nav">
+        <div
+          className="hamburger-btn"
+          onClick={() => {
+            setShowNav(!showNav);
+          }}
+        >
+          <span className="hamburger"></span>
+        </div>
+        <nav className={showNav ? "nav" : "nav not-visible"}>
           {/* <a href="https://rohan-official-portfolio.netlify.app/">
             <span>Home</span>
           </a> */}
           <a
+            className="navbar-links"
             onClick={() => {
               projectRef.current?.scrollIntoView({ behavior: "smooth" });
+              setShowNav(!showNav);
             }}
           >
             <span>Projects</span>
           </a>
-          <a href={resume} download="RESUME">
+          <a
+            className="navbar-links"
+            href={resume}
+            download="RESUME"
+            onClick={() => {
+              setShowNav(!showNav);
+            }}
+          >
             <span>Resume</span>
           </a>
           <a
+            className="navbar-links"
             onClick={() => {
               contactRef.current?.scrollIntoView({ behavior: "smooth" });
+              setShowNav(!showNav);
             }}
           >
             <span>Contact me</span>
           </a>
-          <Link to="/about" target="_blank">
+          <Link
+            className="navbar-links"
+            to="/about"
+            target="_blank"
+            onClick={() => {
+              setShowNav(!showNav);
+            }}
+          >
             <span>About me</span>
           </Link>
         </nav>
